3c18298d63ccc23a9e2e6a21cfecbd197024ed6a
Chris Fries <cfries@google.com>
2019-02-25 20:59:08 +0000

    sm6250-common: Set VENDOR_SECURITY_PATCH to platform
    Set the VENDOR_SECURITY_PATCH level to match the platform
    security patch level. These are functionally the same.
    
    Bug: 125914443
    Change-Id: Ib77ab115d6faabd21958c34e2dae49ec96421fdd
    Signed-off-by: Chris Fries <cfries@google.com>
    Signed-off-by: Abhishek001konni <Abhishek001konni@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -193,7 +193,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2021-10-01
+VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # Sepolicy
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
