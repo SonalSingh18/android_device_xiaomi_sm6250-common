5a7a2361f2db0b1d3a23b6dc3d317dc6141b40c0
Sonal Singh <sonal.singh.19993@gmail.com>
2020-08-14 19:51:15 +0530

    sm6250-common: lineage -> xtended
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- common.mk
+++ common.mk
@@ -290,7 +290,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-xtended
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
---
+++ vendorsetup.sh
@@ -0,0 +1,5 @@
+rm -rf hardware/qcom-caf/sm8150
+git clone https://github.com/SonalSingh18/R-Hals.git -b display-8150 hardware/qcom-caf/sm8150/display
+git clone https://github.com/SonalSingh18/R-Hals.git -b audio-8150 hardware/qcom-caf/sm8150/audio
+git clone https://github.com/SonalSingh18/R-Hals.git -b media-8150 hardware/qcom-caf/sm8150/media
+git clone https://github.com/kdrag0n/proton-clang -b master prebuilts/clang/host/linux-x86/clang-12
