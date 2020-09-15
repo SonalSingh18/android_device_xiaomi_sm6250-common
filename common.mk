6d60959f7e5f36bcecebbf49440ceec908bdd98b
Bharat Pawar <bpawar@codeaurora.org>
2020-09-15 10:49:53 +0530

    sm6250-common: Enable servicetracker V1.2 for legacy targets
    Change-Id: Iafb9b12ef22dc779fd45f0d484dd92340670b010
    Signed-off-by: Abhishek001konni <Abhishek001konni@gmail.com>

--- common.mk
+++ common.mk
@@ -385,6 +385,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepcounter.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml
 
+#servicetracker HAL
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.servicetracker@1.2-impl \
+    vendor.qti.hardware.servicetracker@1.2-service
+
 # NeuralNetworks
 PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.3.vendor
