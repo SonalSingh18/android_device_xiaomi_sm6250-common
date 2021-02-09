48d7e5814052e4e3288ba94f656615639ac6d06e
SonalSingh18 <sonal.singh.19993@gmail.com>
2021-02-09 07:13:42 +0000

    miatoll: xtended -> dot

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2020 The Project-Xtended
+# Copyright (C) 2020 The DotOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/xtended_miatoll.mk
+    $(LOCAL_DIR)/dot_miatoll.mk
 
 COMMON_LUNCH_CHOICES := \
-    xtended_miatoll-user \
-    xtended_miatoll-userdebug \
-    xtended_miatoll-eng
+    dot_miatoll-user \
+    dot_miatoll-userdebug \
+    dot_miatoll-eng
--- dot_miatoll.mk
+++ dot_miatoll.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 The Project-Xtended
+# Copyright (C) 2020 The DotOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -7,23 +7,17 @@
 # Inherit from miatoll  device
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 
-# Inherit some common Xtended stuff
-$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+# Inherit some common DotOS stuff
+$(call inherit-product, vendor/dot/config/common.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# Official
-XTENDED_BUILD_TYPE := OFFICIAL
-
 # Device identifier
-PRODUCT_NAME := xtended_miatoll
+PRODUCT_NAME := dot_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Miatoll
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# Maintainer
-XTENDED_MAINTAINER := 🔥SonalSingh18🔥
