a69190ba55fdecc049beddf46611ecca68f0ed91
SonalSingh18 <sonal.singh.19993@gmail.com>
2021-02-09 07:03:17 +0000

    sm6250-common: xtended -> dot

--- common.mk
+++ common.mk
@@ -289,7 +289,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-xtended
+    $(LOCAL_PATH)/overlay-dot
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
