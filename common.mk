6afd017deaf114f26bdec870a598436b8b93f007
Luckvoltia <sonal.singh.19993@gmail.com>
2021-03-01 21:03:54 +0530

    sm6250-common: arrow -> havoc

--- common.mk
+++ common.mk
@@ -292,7 +292,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-arrow
+    $(LOCAL_PATH)/overlay-havoc
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
