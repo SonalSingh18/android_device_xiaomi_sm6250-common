ec3feb9a896cd76ba728d5326eb45fbfe43094a1
Sonal Singh <sonal.singh.19993@gmail.com>
2021-03-05 13:49:44 +0000

    sm6250-common: Debloat some more gapps

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt CarrierSetup MyVerizonServices OBDM_Permissions OemDmTrigger Showcase SprintDM SprintHM VzwOmaTrigger VZWAPNLib VzwOmaTrigger obdm_stub
+LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt CalculatorGooglePrebuilt CalendarGooglePrebuilt Chrome Chrome-Stub DevicePolicyPrebuilt Maps MicropaperPrebuilt NgaResources Ornament Photos PixelWallpapers2020 PrebuiltDeskClockGoogle PrebuiltGmail SoundAmplifierPrebuilt TrichromeLibrary TrichromeLibrary-Stub Tycho WallpapersBReel2020 CarrierSetup MyVerizonServices OBDM_Permissions OemDmTrigger Showcase SprintDM SprintHM talkback arcore AndroidAutoStubPrebuilt VzwOmaTrigger RecorderPrebuilt Velvet VZWAPNLib VzwOmaTrigger obdm_stub
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
