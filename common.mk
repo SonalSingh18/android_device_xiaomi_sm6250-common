a5da494c21232753d209080b7105223633a49f5a
tushar <tusharredekar007@gmail.com>
2021-10-23 21:35:53 +0530

    sm6250-common: build android.hidl.memory.block@1.0.vendor

--- common.mk
+++ common.mk
@@ -231,7 +231,8 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0.vendor \
     android.hidl.manager@1.0 \
-    android.hidl.manager@1.0.vendor
+    android.hidl.manager@1.0.vendor \
+    android.hidl.memory.block@1.0.vendor
 
 PRODUCT_PACKAGES += \
     libhidltransport \
