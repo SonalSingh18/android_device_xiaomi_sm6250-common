f5da7adf7f36e83212e279b5364d5447658ecfc0
tushar <tusharredekar007@gmail.com>
2021-10-28 13:45:48 +0530

    sm6250-common: Initial changes for Kraken

--- common.mk
+++ common.mk
@@ -279,10 +279,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_sm6250
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_PACKAGES += \
     libmm-omxcore \
