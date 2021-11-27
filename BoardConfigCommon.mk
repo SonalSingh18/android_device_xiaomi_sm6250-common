ff1f01136a59228be1f7742743ec00596784f7d1
tushar <tusharredekar007@gmail.com>
2021-11-27 10:14:22 +0530

    sm6250-common: update blobs from gram-V12.5.2.0.RJPINXM

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -193,7 +193,7 @@ OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2021-08-01
+VENDOR_SECURITY_PATCH := 2021-10-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
