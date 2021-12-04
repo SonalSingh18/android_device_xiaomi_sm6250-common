672f2cea0e491f8af49c85918afc364fe3e7d434
YumeMichi <do4suki@gmail.com>
2021-12-04 12:29:52 +0000

    sm6250-common: Switch to QTI bt component
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- common.mk
+++ common.mk
@@ -60,11 +60,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
-# Component override
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml \
-    $(LOCAL_PATH)/configs/component-overrides_qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/component-overrides.xml
-
 # FM
 PRODUCT_PACKAGES += \
     FM2 \
@@ -89,10 +84,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
-    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
@@ -326,6 +317,8 @@ PRODUCT_PACKAGES += \
 
 # QTI components
 TARGET_BOARD_PLATFORM := atoll
+TARGET_COMMON_QTI_COMPONENTS := \
+    bt
 
 # RCS
 PRODUCT_PACKAGES += \
