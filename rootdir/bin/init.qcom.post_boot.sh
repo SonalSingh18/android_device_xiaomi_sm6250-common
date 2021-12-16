70fcaa8bd3ccfbfacedc52a475723f79c916f508
tushar <tusharredekar007@gmail.com>
2021-12-16 15:40:25 +0530

    sm6250-common: rootdir: drop foreground boost cpus

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -248,7 +248,6 @@ function configure_memory_parameters() {
     # cpuset parameters
     echo 0-2     > /dev/cpuset/background/cpus
     echo 0-3     > /dev/cpuset/system-background/cpus
-    echo 4-7     > /dev/cpuset/foreground/boost/cpus
     echo 0-2,4-7 > /dev/cpuset/foreground/cpus
     echo 0-7     > /dev/cpuset/top-app/cpus
 
