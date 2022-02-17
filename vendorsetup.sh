160aa3b612b0f530ed06c058d83d73673cac8a01
Abhishek001konni <abhishek001konni@gmail.com>
2022-02-17 15:22:51 +0530

    sm6250-common: Add vendorsetup

---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+rm -rf hardware/xiaomi
+git clone https://github.com/ArrowOS-Devices/android_hardware_xiaomi.git -b arrow-12.0 hardware/xiaomi
+git clone https://github.com/ArrowOS-Devices/android_device_xiaomi_extras.git -b arrow-12.0 device/xiaomi/extras
