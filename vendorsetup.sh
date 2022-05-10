d84e31f88ab1a599b6b8fbe42f573a9d5aaf4454
Abhishek001konni <Abhishek001konni@gmail.com>
2022-05-10 13:11:36 +0530

    sm6250-common: Switch branch of hardware/xiaomi
    Signed-off-by: Abhishek001konni <Abhishek001konni@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
 rm -rf hardware/xiaomi
-git clone https://github.com/ArrowOS-Devices/android_hardware_xiaomi.git -b arrow-12.0 hardware/xiaomi
+git clone https://github.com/ArrowOS-Devices/android_hardware_xiaomi.git -b arrow-12.1 hardware/xiaomi
 git clone https://github.com/ArrowOS-Devices/android_device_xiaomi_extras.git -b arrow-12.0 device/xiaomi/extras
 git clone https://gitlab.com/ImSpiDy/azure-clang.git -b clang-14 prebuilts/clang/host/linux-x86/clang-14.0 --depth=1
