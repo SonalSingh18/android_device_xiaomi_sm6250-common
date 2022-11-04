0d974e96b2c77fa6d74f802ce5790e7c6f042c4a
Sonal Singh <sonal.singh.19993@gmail.com>
2022-11-04 17:45:51 +0000

    [SQUASH] sm6250-common: arrow -> evolution
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    
    sm6250-common: switch TARGET_2ND_ARCH_VARIANT to armv8-2a
    
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    
    sm6250-common: Remove unwanted apps
    
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    
    Revert "sm6250-common: rro_overlay: Add Notch Bar Killer"
    
    This reverts commit 946651c40c5e9f70ca4af5c1bb65fc029217b47f.
    
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    
    sm6250-common: overlay: correct SystemUI apk name and path
    
    apk name and path was changed as seen here
    https://github.com/PixelExperience/vendor_pixel-framework/commit/8be4fc80c0af1a38386b7dc23b7392d0a9e64669
    
    This causes errors in logs since pinner was trying to pin old app.
    
    PinnerService: Could not pin file /system_ext/priv-app/SystemUI/SystemUI.apk
    PinnerService: Failed to pin file = /system_ext/priv-app/SystemUI/SystemUI.apk
    
    Change-Id: I609b29b9139d5cfaf1248c9ac001c280ae10c31c
    
    sm6250-common: overlay: Enable boost framework
    
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    
    sm6250-common: overlay: Enable smart charging
    
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    
    sm6250-common: overlay: Enable showing optional build status
    
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    
    sm6250-common: properties: Set Brightness slider curve Implementation
    
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- common.mk
+++ common.mk
@@ -133,10 +133,6 @@ PRODUCT_COPY_FILES += \
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
-# Dexpreopt
-PRODUCT_DEXPREOPT_SPEED_APPS += \
-    SystemUI
-
 # Display
 PRODUCT_PACKAGES += \
     disable_configstore \
@@ -365,7 +361,6 @@ PRODUCT_PACKAGES += \
    CarrierConfigOverlay \
    DialerOverlay \
    FrameworksResOverlay \
-   NotchBarKillerOverlay \
    SettingsOverlay \
    SettingsProviderOverlay \
    SystemUIOverlay \
@@ -411,6 +406,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.recovery.qcom.rc:recovery/root/init.recovery.qcom.rc \
     $(LOCAL_PATH)/rootdir/bin/init.recovery.qcom.sh:recovery/root/init.recovery.qcom.sh
+
+# RemovePackages
+PRODUCT_PACKAGES += \
+    RemovePackages
 
 # RIL
 PRODUCT_PACKAGES += \
