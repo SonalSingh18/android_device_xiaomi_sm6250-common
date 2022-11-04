55dc03a4438d91ecb9f09564dbe8ee8259cf370a
Sonal Singh <sonal.singh.19993@gmail.com>
2022-11-04 16:37:32 +0000

    sm6250-common: Drop MiuiCamera
     - does'nt work yet
    
    This reverts commit 6e5ced47138b7299bc5a9cdf9b631b48101cdb08.
    
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- common.mk
+++ common.mk
@@ -323,9 +323,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
-# MiuiCamera
-$(call inherit-product-if-exists, vendor/xiaomi/miuicamera/config.mk)
-
 # Net
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.1.vendor \
