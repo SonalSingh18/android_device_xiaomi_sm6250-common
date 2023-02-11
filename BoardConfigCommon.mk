12098a36d0df9be62fcd3a0f1bcbcac126807e61
Sonal Singh <sonal.singh.19993@gmail.com>
2023-02-11 21:44:35 +0530

    [SQUASH] sm6250-common: Drop current init implementation
    Revert "sm6250-common: libinit: Migrate system/core/base to system/libbase."
    
    This reverts commit 4b2cbc7ee3797ae27afc052786202007937972ac.
    
    Revert "sm6250-common: Adapt libinit to Android 11"
    
    This reverts commit 1ea5b096b43460cbc61e8bd11596989df659c408.
    
    Revert "sm6250-common: init: cleanup includes"
    
    This reverts commit f4ce900e015894d024510c366c6b85f71bd74198.
    
    Revert "sm6250-common: init: split up functionality into separate static library"
    
    This reverts commit 3b12ba11d07c9d9f9ad20c6ac5c75cc2ea3202c8.
    
    Revert "sm6250-common: init: group dalvik property initialization together"
    
    This reverts commit a0c7e0c3c94f51f81a17e40bcad3e54a0494f1f8.
    
    Revert "sm6250-common: init: override dalvik heap config based on total RAM"
    
    This reverts commit e07fbb9738330a46d6fcf60defa1912a76277151.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -102,10 +102,6 @@ BOARD_KERNEL_CMDLINE += kpti=off
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/configs/vintf/compatibility_matrix.xml
 
-# Init
-TARGET_INIT_VENDOR_LIB := //$(COMMON_PATH):libinit_xiaomi_sm6250
-TARGET_RECOVERY_DEVICE_MODULES := libinit_xiaomi_sm6250
-
 # Media
 TARGET_USES_ION := true
 TARGET_DISABLED_UBWC := true
