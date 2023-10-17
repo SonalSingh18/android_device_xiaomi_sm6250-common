938f88d7cc450af85e88450bec836f9fb1aa9e9f
userariii <abhattacharjee717@gmail.com>
2023-10-17 22:21:01 +0530

    sm6250-common: Remove Camera legacy
    Signed-off-by: userariii <abhattacharjee717@gmail.com>

--- common.mk
+++ common.mk
@@ -107,8 +107,6 @@ PRODUCT_PACKAGES += \
     libstdc++.vendor
 
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64 \
     libdng_sdk.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor
 
