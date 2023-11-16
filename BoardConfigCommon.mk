27e69cb5241702b6ac4dd73e7ae5a0062f965925
Sonal Singh <sonal.singh.19993@gmail.com>
2023-11-17 00:50:07 +0530

    sm6250-common: Add device_framework_matrix.xml to satisfy FCM vintf check
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -71,6 +71,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
+    $(COMMON_PATH)/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 
