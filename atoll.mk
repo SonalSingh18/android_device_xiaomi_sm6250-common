cd84edf91adab92ca5df1d759baa870b71677511
Sonal Singh <sonal.singh.19993@gmail.com>
2023-11-19 02:03:16 +0530

    sm6250-common: Include ViPER4AndroidFX
     * Source - https://github.com/swiitch-OFF-Lab/packages_apps_ViPER4AndroidFX.git
    
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- atoll.mk
+++ atoll.mk
@@ -16,6 +16,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 # MiuiCamera
 $(call inherit-product-if-exists, vendor/xiaomi/miuicamera/config.mk)
 
+# ViPER4AndroidFX
+$(call inherit-product-if-exists, packages/apps/ViPER4AndroidFX/config.mk)
+
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
